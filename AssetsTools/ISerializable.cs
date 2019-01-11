d6eb8af7533fe70bba310d8f50778081b7bd1bf0
grainrigi <naonaokiryu2@gmail.com>
2019-01-11 17:10:44 +0900

    Add Array Extension for ISerializable

--- AssetsTools/ISerializable.cs
+++ AssetsTools/ISerializable.cs
@@ -11,6 +11,18 @@ namespace AssetsTools {
         void Write(UnityBinaryWriter writer);
     }
 
+    public static class ISerializableArrayExtensions {
+        public static void Read<T>(this T[] ary, UnityBinaryReader reader) where T : ISerializable {
+            for (int i = 0; i < ary.Length; i++)
+                ary[i].Read(reader);
+        }
+
+        public static void Write<T>(this T[] ary, UnityBinaryWriter writer) where T : ISerializable {
+            for (int i = 0; i < ary.Length; i++)
+                ary[i].Write(writer);
+        }
+    }
+
     public class UnknownFormatException : Exception {
         public UnknownFormatException() { }
         public UnknownFormatException(string message) : base(message) { }
