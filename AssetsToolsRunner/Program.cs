0899d6489f5829520b5ede789b8a4a60a62f147d
grainrigi <naonaokiryu2@gmail.com>
2019-01-11 17:12:38 +0900

    Add AssetsToolsRunner

---
+++ AssetsToolsRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetsTools;
+using System.IO;
+
+namespace AssetsToolsRunner {
+    class Program {
+        static void Main(string[] args) {
+            UnityBinaryReader r = new UnityBinaryReader("test.unity3d");
+            AssetBundleFile bundle = new AssetBundleFile();
+            bundle.Read(r);
+            UnityBinaryWriter w = new UnityBinaryWriter();
+            bundle.EnableCompression = true;
+            bundle.Write(w);
+            byte[] raw = w.ToBytes();
+            using (FileStream fs = new FileStream("test.unity3d.comp", FileMode.Create)) {
+                fs.Write(raw, 0, raw.Length);
+            }
+        }
+    }
+}
