99c83d4cbfb67dd8e88ef35aa063fe66f5b564c3
grainrigi <naonaokiryu2@gmail.com>
2019-01-14 15:42:45 +0900

    Move SerializedType to AssetsTools namespace

--- AssetsTools/AssetsFile.Metadata.cs
+++ AssetsTools/AssetsFile.Metadata.cs
@@ -24,35 +24,6 @@ namespace AssetsTools {
             }
         }
 
-        public struct SerializedType : ISerializable {
-            public int ClassID;
-            public bool IsStrippedType; // version >= 16
-            public short ScriptTypeIndex; // version >= 17
-            public byte[] ScriptID;
-            public byte[] OldTypeHash;
-            public TypeTree TypeTree;
-
-            public void Read(UnityBinaryReader reader) {
-                ClassID = reader.ReadInt();
-                IsStrippedType = reader.ReadByte() != 0;
-                ScriptTypeIndex = reader.ReadShort();
-
-                if (ClassID == (int)ClassIDType.MonoBehaviour)
-                    ScriptID = reader.ReadBytes(16);
-                OldTypeHash = reader.ReadBytes(16);
-            }
-
-            public void Write(UnityBinaryWriter writer) {
-                writer.WriteInt(ClassID);
-                writer.WriteByte((byte)(IsStrippedType ? 1 : 0));
-                writer.WriteShort(ScriptTypeIndex);
-
-                if(ClassID == (int)ClassIDType.MonoBehaviour)
-                    writer.WriteBytes(ScriptID, 0, 16);
-                writer.WriteBytes(OldTypeHash, 0, 16);
-            }
-        }
-
         private void readMetadata(UnityBinaryReader reader) {
             // Read Header
             MetadataHeader.Read(reader);
---
+++ AssetsTools/SerializedType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetsTools {
+    public struct SerializedType : ISerializable {
+        public int ClassID;
+        public bool IsStrippedType; // version >= 16
+        public short ScriptTypeIndex; // version >= 17
+        public byte[] ScriptID;
+        public byte[] OldTypeHash;
+        public TypeTree TypeTree;
+
+        public void Read(UnityBinaryReader reader) {
+            ClassID = reader.ReadInt();
+            IsStrippedType = reader.ReadByte() != 0;
+            ScriptTypeIndex = reader.ReadShort();
+
+            if (ClassID == (int)ClassIDType.MonoBehaviour)
+                ScriptID = reader.ReadBytes(16);
+            OldTypeHash = reader.ReadBytes(16);
+        }
+
+        public void Write(UnityBinaryWriter writer) {
+            writer.WriteInt(ClassID);
+            writer.WriteByte((byte)(IsStrippedType ? 1 : 0));
+            writer.WriteShort(ScriptTypeIndex);
+
+            if (ClassID == (int)ClassIDType.MonoBehaviour)
+                writer.WriteBytes(ScriptID, 0, 16);
+            writer.WriteBytes(OldTypeHash, 0, 16);
+        }
+    }
+}
