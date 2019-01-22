60c89bb13bb093b977cf1f32159c203f9c11b290
grainrigi <naonaokiryu2@gmail.com>
2019-01-22 23:40:38 +0900

    Implement MonoBehaviour (De)Serializer Chache

--- AssetsTools/DynamicAsset.Cache.cs
+++ AssetsTools/DynamicAsset.Cache.cs
@@ -9,25 +9,49 @@ namespace AssetsTools {
         internal static Dictionary<string, DynamicAsset> PrototypeDic = new Dictionary<string, DynamicAsset>();
 
         private static Dictionary<int, Func<UnityBinaryReader, DynamicAsset>> _deserializerCache = new Dictionary<int, Func<UnityBinaryReader, DynamicAsset>>();
+        private static Dictionary<int, Func<UnityBinaryReader, DynamicAsset>> _monodeserializerCache = new Dictionary<int, Func<UnityBinaryReader, DynamicAsset>>();
         internal static Func<UnityBinaryReader, DynamicAsset> GetDeserializer(SerializedType type) {
-            if (_deserializerCache.TryGetValue(type.ClassID, out var func))
+            var dic = _deserializerCache;
+            int id = type.ClassID;
+            if(type.ClassID == (int)ClassIDType.MonoBehaviour) {
+                dic = _monodeserializerCache;
+                id = GetHashOfMonoBehaviour(type.ScriptID);
+            }
+
+            if (dic.TryGetValue(id, out var func))
                 return func;
             else {
                 var des = GenDeserializer(type.TypeTree.Nodes);
-                _deserializerCache.Add(type.ClassID, des);
+                dic.Add(id, des);
                 return des;
             }
         }
 
         private static Dictionary<int, Action<UnityBinaryWriter, DynamicAsset>> _serializerCache = new Dictionary<int, Action<UnityBinaryWriter, DynamicAsset>>();
+        private static Dictionary<int, Action<UnityBinaryWriter, DynamicAsset>> _monoserializerCache = new Dictionary<int, Action<UnityBinaryWriter, DynamicAsset>>();
         internal static Action<UnityBinaryWriter, DynamicAsset> GetSerializer(SerializedType type) {
-            if (_serializerCache.TryGetValue(type.ClassID, out var func))
+            var dic = _serializerCache;
+            int id = type.ClassID;
+            if(type.ClassID == (int)ClassIDType.MonoBehaviour) {
+                dic = _monoserializerCache;
+                id = GetHashOfMonoBehaviour(type.ScriptID);
+            }
+
+            if (dic.TryGetValue(id, out var func))
                 return func;
             else {
                 var ser = GenSerializer(type.TypeTree.Nodes);
-                _serializerCache.Add(type.ClassID, ser);
+                dic.Add(id, ser);
                 return ser;
             }
+        }
+
+        private static int GetHashOfMonoBehaviour(byte[] scriptID) {
+            int hash = BitConverter.ToInt32(scriptID, 0);
+            hash ^= BitConverter.ToInt32(scriptID, 4);
+            hash ^= BitConverter.ToInt32(scriptID, 8);
+            hash ^= BitConverter.ToInt32(scriptID, 12);
+            return hash;
         }
     }
 }
